5eb713df01448b833bacef6917709b5e3c9cbd71
Younis Rahman <younisrahman007@gmail.com>
2022-02-09 21:15:52 +0600

    complete for submission

--- __tests__/Page-test.tsx
+++ __tests__/Page-test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Index from '../src/components/Page/index';
+// import Index from '../src/components/Page/index';
+import Index from '../src/components/Info/index';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<Index index={1} translateX={9} />).toJSON();
+  const tree = renderer.create(<Index />).toJSON();
   expect(tree).toMatchSnapshot();
 });
